df9b585d70f66b2ba93ef702d36e70750d7b6d9d
LeandroMLS <80489021+LeandroMLS@users.noreply.github.com>
2021-03-18 14:20:24 -0300

    Primeira versão da aplicação

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function atribuirNome() {
   if ($reservarInput.value) {
     reservar($reservarInput.value);
 
-    $reservarInput.value = ';
+    $reservarInput.value = '';
 
     return closeModal();
   }
@@ -75,35 +75,4 @@ function closeModal() {
   if (/show/.test($modal.className)) {
     $modal.className = 'modal';
   }
-}
-=======
-const assentos = [
-    {id: 'A1,' reservado: false, nome: null},
-    {id: 'A2,' reservado: false, nome: null},
-    {id: 'A3,' reservado: false, nome: null},
-    {id: 'A4,' reservado: false, nome: null},
-    {id: 'A5,' reservado: false, nome: null},
-    {id: 'A6,' reservado: false, nome: null},
-    {id: 'A7,' reservado: false, nome: null},
-    {id: 'A8,' reservado: false, nome: null},
-    {id: 'A9,' reservado: false, nome: null},
-    {id: 'A10', reservado: false, nome: null},
-    {id: 'A11', reservado: false, nome: null},
-    {id: 'A12', reservado: false, nome: null},
-    {id: 'A13', reservado: false, nome: null},
-    {id: 'A14', reservado: false, nome: null},
-    {id: 'A15', reservado: false, nome: null},
-    {id: 'A16', reservado: false, nome: null},
-    {id: 'A17', reservado: false, nome: null},
-    {id: 'A18', reservado: false, nome: null},
-    {id: 'A19', reservado: false, nome: null},
-    {id: 'A20', reservado: false, nome: null},
-    {id: 'A21', reservado: false, nome: null},
-    {id: 'A22', reservado: false, nome: null},
-    {id: 'A23', reservado: false, nome: null}
-]
-
-function reservar(event) {
-
-    alert('Assento reservado')
 }
